9058872ec064c21ef7bd2e34d346551a47501c31
zaigiaz <shayne.tman@gmail.com>
2026-09-05 14:08:43 -0500

    added arena allocation to handle allocating multiple objects. working on game-loop

--- src/main.c
+++ src/main.c
@@ -3,8 +3,19 @@
 #include <time.h>
 #include "raylib.h"
 
+#define ARENA_IMPLEMENTATION
+#include "arena.h"
+
+#define TEST_GAME 1
 #define MAX_MOVES 5
 #define MAX_TYPE_COUNT 5
+
+typedef enum {
+  WIN,
+  RUNNING,
+  LOSE,
+  ERROR
+} GAME_STATE;
 
 typedef enum {
   MOVE_PHYSICAL,
@@ -18,12 +29,12 @@ typedef enum {
 
 // first element is elem being affected by second element
 const float type_interaction_table[6][6] = {
-    [MOVE_PHYSICAL] = {0.0f, 2.0f, 1.5f, 1.0f, 1.5f, 1.0f},
-    [MOVE_FLAME]    = {3.0f, 0.0f, 2.0f, 1.5f, 1.1f, 1.0f},
-    [MOVE_STORM]    = {1.0f, 1.0f, 0.0f, 1.0f, 1.4f, 1.5f},
-    [MOVE_EARTH]    = {1.0f, 1.8f, 1.0f, 0.0f, 1.2f, 1.0f},
-    [MOVE_WHITE]    = {2.0f, 1.0f, 2.0f, 1.0f, 0.0f, 1.5f},
-    [MOVE_BLACK]    = {1.0f, 2.0f, 1.0f, 1.0f, 1.0f, 0.0f}
+  [MOVE_PHYSICAL] = {0.0f, 2.0f, 1.5f, 1.0f, 1.5f, 1.0f},
+  [MOVE_FLAME]    = {3.0f, 0.0f, 2.0f, 1.5f, 1.1f, 1.0f},
+  [MOVE_STORM]    = {1.0f, 1.0f, 0.0f, 1.0f, 1.4f, 1.5f},
+  [MOVE_EARTH]    = {1.0f, 1.8f, 1.0f, 0.0f, 1.2f, 1.0f},
+  [MOVE_WHITE]    = {2.0f, 1.0f, 2.0f, 1.0f, 0.0f, 1.5f},
+  [MOVE_BLACK]    = {1.0f, 2.0f, 1.0f, 1.0f, 1.0f, 0.0f}
 };
 
 // change move_type enum to char pointer
@@ -36,7 +47,7 @@ char* mv_type_to_string(move_type type) {
   case MOVE_WHITE: mv_name = "White"; break;
   case MOVE_BLACK: mv_name = "Black"; break;
   case MOVE_EARTH: mv_name = "Earth"; break;
-  default: fprintf(stderr, "move doesn't exist"); exit(1);
+  default: fprintf(stderr, "move doesn't exist"); exit(1); // TODO :: have better error defaulting
   }
   return mv_name;
 }
@@ -55,10 +66,8 @@ typedef struct {
   int MP;
   int Strength;
   int Intelligence;
-  int Endurance;
-  
+  int Endurance;  
 } stats;
-
 
 // character sheet for a player or entity
 typedef struct {
@@ -93,13 +102,16 @@ void prt_char_info(player* sheet) {
 void proc_char(player* sheet, char* ent_name) {
   // TODO :: figure out a way to add moves without having to figure out count, etc. (store in file)
   // TODO :: make it procgen for generating a character
-  sheet->name = ent_name;
-  
-  /* int random = GetRandomValue(5, 15); */
-  /* printf("%d", random); */
-  
-  sheet->charstats = (stats) { .HP=40,       .AP=40,           .MP=40,
-			       .Strength=10, .Intelligence=10, .Endurance=10, };
+  sheet->name = ent_name;  
+  sheet->player_type = MOVE_PHYSICAL;
+  int rand[6];
+
+  for(size_t i=0; i<6; i++) {
+    rand[i] = GetRandomValue(1, 50);
+  }
+
+  sheet->charstats = (stats) { .HP=rand[0],       .AP=rand[1],           .MP=rand[2],
+			       .Strength=rand[3], .Intelligence=rand[4], .Endurance=rand[5], };
   sheet->moves[0] =  (Move)  { .name="Tackle", .damage=10 };
   sheet->moves[1] =  (Move)  { .name="Scratch", .damage=20 };
   sheet->current_moves = 2;
@@ -108,30 +120,61 @@ void proc_char(player* sheet, char* ent_name) {
 // function to get two entities to interact through move system
 void attack(player* ent, Move attack) {
   float modifier = type_interaction_table[ent->player_type][attack.type];
-  ent->charstats.HP -= (attack.damage * modifier);
+  int damage = (attack.damage * modifier);
+  ent->charstats.HP -= damage;
+  printf("%s was attacked by %s for %d damage\n", ent->name, attack.name, damage);
 }
 
-// main
-// TODO :: make main game loop of single battle with data I currently have
-int main() {
-
-  printf("entering game\n");
-  SetRandomSeed((unsigned int)time(NULL));
-  
+void test_game() {
   player *protag, *enem;
   protag = malloc(sizeof(player));
   enem = malloc(sizeof(player));
 
   proc_char(protag, "protag");
   proc_char(enem, "enem");
-  
-  attack(protag, (Move){"hi", 10, MOVE_FLAME});
-
   prt_char_info(protag);
   prt_char_info(enem);
+  
+  attack(protag, (Move){"ember", 10, MOVE_FLAME});
 
   free(protag);
   free(enem);
+}
+
+// main
+// TODO :: make main game loop of single battle with data I currently have
+int main() {
+
+  SetRandomSeed((unsigned int)time(NULL));
+
+  printf("entering game\n");
+
+  if(TEST_GAME == 1) {
+    test_game();
+    exit(0);
+  }
+
+  Arena game_arena = {0};
+
+  player *protag, *enem;
+  protag = arena_alloc(&game_arena, sizeof(player));
+  enem = arena_alloc(&game_arena, sizeof(player));
+
+  proc_char(protag, "protag");
+  proc_char(enem, "enem");
+
+  GAME_STATE state = RUNNING;
+  while(state == RUNNING) { 
+  
+  attack(protag, (Move){"Ember", 5, MOVE_FLAME});
+
+  if(protag->charstats.HP <= 0) {
+    state = LOSE;
+    printf("player has died!\n");
+  }
+ }
+
+  arena_free(&game_arena);
 
   printf("exiting game\n");
   return 0;
